8c214f6acdb9445423dd1404a465e58947426c32
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-10-01 14:00:30 -0300

    Associação Binária aula 01-10-25

---
+++ prjExemploAssociacaoBinaria/src/Aplic.java
@@ -0,0 +1,72 @@
+
+import fatec.poo.model.Departamento;
+import fatec.poo.model.FuncionarioComissionado;
+import fatec.poo.model.FuncionarioHorista;
+import fatec.poo.model.FuncionarioMensalista;
+
+/**
+ *
+ * @author Nicolas Ap
+ */
+public class Aplic {
+    public static void main(String[] args) {
+        FuncionarioHorista funcHor = new FuncionarioHorista(1010,
+                                                            "Pedro Silveira",
+                                                            "14/05/1978",
+                                                            15.80);
+        FuncionarioMensalista funcMen = new FuncionarioMensalista(2020,
+                                                                  "Ana Beatriz", 
+                                                                  "22/10/1997",
+                                                                  600.0);
+        
+        FuncionarioComissionado funcCom = new FuncionarioComissionado(3030, 
+                                                                      "Joao Mendes",
+                                                                      "10/12/1975",
+                                                                      10);
+        
+        
+        Departamento dep1 = new Departamento ("CP", "Compras");
+        Departamento dep2 = new Departamento ("RH", "Recursos Humanos");
+        
+        funcHor.setCargo("Programador");
+        funcMen.setCargo("Aux. Administrativo");
+        funcCom.setCargo("Vendedor");
+     
+        //Estabelece a associação entre um objeto da classse FuncionárioHorista
+        //com um objeto da classe Departamento
+        
+        funcHor.setDepartamento(dep1);
+        System.out.println("O funcionario horista: " + funcHor.getNome() + " trabalha no departamento de: " + funcHor.getDepartamento().getNome());
+    
+        //Estabelece a associação entre um objeto da classse FuncionárioMensalista
+        //com um objeto da classe Departamento
+        
+        funcMen.setDepartamento(dep1);
+        System.out.println("O funcionário mensalista: " + funcMen.getNome() + " trabalha no departamento de: " + funcMen.getDepartamento().getNome());
+    
+        //Estabelece a associação entre um objeto da classse FuncionárioComissionado
+        //com um objeto da classe Departamento
+        
+        funcCom.setDepartamento(dep2);
+        System.out.println("O funcionário comissionado: " + funcCom.getNome() + " trabalha no departamento de: " + funcCom.getDepartamento().getNome());
+    
+        //Estabelece a associação entre um objeto da classe Departamento com
+        //um objeto das classes FuncionarioHorista e FuncionarioMensalista
+        
+        dep1.addFuncionario(funcHor);
+        
+        dep1.addFuncionario(funcMen);
+        
+        dep1.listarFuncionario();
+    
+        //Estabelece a associação entre um objeto da classe Departamento com
+        //um objeto da classe FuncionarioComissionado
+        
+        dep2.addFuncionario(funcCom);
+        
+        dep2.listarFuncionario();
+      
+    
+    }    
+}
+
---
+++ prjExemploAssociacaoBinaria/src/fatec/poo/model/Funcionario.java
@@ -0,0 +1,57 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Nicolas Ap
+ */
+abstract public class Funcionario {
+    private int registro;
+    private String nome;
+    private String dtAdmissao;  
+    private String cargo;
+    private Departamento departamento; //Ponteiro que armazena o endereço de um objeto da classe Departamento
+    
+    public Funcionario(int r, String n, String dtAdm){
+        registro = r;
+        nome = n;
+        dtAdmissao = dtAdm;  
+    }   
+    
+    public int getRegistro(){
+        return(registro);
+    }
+    public String getNome(){
+        return(nome);
+    }
+    public String getDtAdmissao(){
+        return(dtAdmissao);
+    }
+    public String getCargo(){
+        return(cargo);
+    }
+    public void setCargo(String c){
+        cargo = c;
+    }
+    
+    public abstract double calcSalBruto();
+    
+    public double calcDesconto(){
+        return(0.10 * calcSalBruto());
+    }
+    public double calcSalLiquido(){
+        return(calcSalBruto() - calcDesconto());
+    }
+
+    
+    //Retorna o endereço de um objeto da classe Departamento
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    //Tem como parâmetro de entrada o endereço de um objeto da classe Departamento
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    
+    
+}
